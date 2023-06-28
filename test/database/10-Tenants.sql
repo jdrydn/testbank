949e2da8c86a738028781509c374c15204b973ee
jdrydn <james@jdrydn.com>
2023-06-28 22:32:34 +0100

    fix: Updated SQL tables

--- test/database/10-Tenants.sql
+++ test/database/10-Tenants.sql
@@ -13,4 +13,4 @@ CREATE TABLE IF NOT EXISTS `Tenants` (
   PRIMARY KEY (`id`),
   UNIQUE KEY `email` (`email`),
   KEY `createdAt` (`createdAt`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci AUTO_INCREMENT=10;
